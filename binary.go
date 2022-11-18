9088190a2c644c63615531fbc43bfa7d7b9ebdd9
chuqq <chuqingq@qq.com>
2022-11-18 09:53:13 +0800

    用binary.Write/Read即可完成功能

--- binary.go
+++ binary.go
@@ -13,6 +13,7 @@ func ToBinary(vs ...interface{}) []byte {
 	return b.Bytes()
 }
 
+// TODO 其实可以直接用binary.Write/Read(buf, order, &[]interface{struct{}, struct{}})方式来读取
 // func FromBinary(b []byte, vs ...interface{}) {
 // 	buf := bytes.NewBuffer(b)
 // 	for _, v := range vs
