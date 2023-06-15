979d3de12084e63fff457475448b2db56eed9e3e
chuqq <chuqingq@qq.com>
2023-06-15 14:58:02 +0800

    go.mod

--- strings_test.go
+++ strings_test.go
@@ -4,39 +4,33 @@ import (
 	"encoding/hex"
 	"log"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestCapture(t *testing.T) {
 	a := "a,b,c"
 	r := Capture(a, ",", ",")
-	if r != "b" {
-		t.Fatalf("Capture() error")
-	}
+	assert.Equal(t, r, "b")
 }
 
 func TestCapture2(t *testing.T) {
 	a := "a,b,c"
 	r := Capture(a, "^", "c")
 	log.Printf("r: %v", r)
-	if r != "a,b," {
-		t.Fatalf("Capture() error")
-	}
+	assert.Equal(t, r, "a,b,")
+
 	r = Capture(a, "b", "$")
-	if r != ",c" {
-		t.Fatalf("capture 2.2 error")
-	}
+	assert.Equal(t, r, ",c")
 }
 
 func TestCapture3(t *testing.T) {
 	a := "total 0\nlrwxrwxrwx 1 root root 13 Jul 21 10:45 mmc-G8GTF4R_0x74dbd49e -> ./../mmcblk2\n"
 	r := Capture(a, "0x", " ")
-	if r != "74dbd49e" {
-		t.Fatalf("capture result not expected")
-	}
+	assert.Equal(t, r, "74dbd49e")
+
 	log.Printf("r: %v", r)
 	h, err := hex.DecodeString(r)
 	log.Printf("h: %v, %v", h, err)
-	if err != nil {
-		t.Fatalf("hex decode error: %v", err)
-	}
+	assert.NoError(t, err)
 }
